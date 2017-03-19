9da37c84c268d7946b1ff345608de9a5b9200489
wilson496 <cw4886130@gmail.com>
2017-03-18 17:39:23 -0700

    Completed codecommiter and updated launcher

--- launcher.py
+++ launcher.py
@@ -1,6 +1,35 @@
 import boto3
 
-client = boto3.client('elasticbeanstalk')
+def main():
+
+	bean_client = boto3.client('elasticbeanstalk')
+	code_client = boto3.client('codecommit')
 
 
 
+	repo_info = code_client.get_repository(
+   		repositoryName='mojio-amazon'
+	)
+
+	print(repo_info)
+
+
+
+	response = bean_client.create_application_version(
+    	ApplicationName='my-app',
+    	AutoCreateApplication=True,
+    	Description='my-app-v1',
+    	Process=True,
+    	SourceBuildInformation={
+        	'SourceType': 'Git',
+        	'SourceRepository': 'CodeCommit',
+        	'SourceLocation': 'mojio-amazon'
+    	},
+		VersionLabel='v1',
+	)
+
+	print(response)
+
+
+if __name__ == "__main__":
+	main()
